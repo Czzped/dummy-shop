5b8dea827df265abcea64e42593f711ddf60d87f
Pedro Henrique <phcosta069@gmail.com>
2023-11-02 20:02:53 -0300

    refactor: Footer import in no use removed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,9 @@
-import { Link } from "react-router-dom"
-
 export function Footer() {
     return (
         <footer>
             <span>
-                Made With ❤️ By <Link to={"https://github.com/Czzped"} target="_blank">Pedro Henrique</Link>
+                Made With ❤️ By <a href="https://github.com/Czzped" target="_blank">Pedro Henrique</a>
             </span>
-        </footer>
+        </footer >
     )
 }
