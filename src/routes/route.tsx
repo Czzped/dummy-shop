4a93140e0c76619acc7293cda954478d34e72047
Pedro Henrique <phcosta069@gmail.com>
2023-11-03 12:16:37 -0300

    refactor: pages index name refactored p2

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import { DefaultLayout } from "../layout/DefaultLayout";
-import { Home } from "../pages/Home/Home";
-import { Cart } from "../pages/Cart/Cart";
-import { Product } from "../pages/Product/Product";
+import { Home } from "../pages/Home";
+import { Product } from "../pages/Product";
+import { Cart } from "../pages/Cart";
 
 const router = createBrowserRouter([
     {
