bb70abaf07e2aa4a4a5eabd12ca3fbc7ae29edfb
Pedro Henrique <phcosta069@gmail.com>
2023-11-02 22:35:05 -0300

    feat: DefaultLayout Outlet component include

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -1,10 +1,14 @@
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
+import { Outlet } from "react-router-dom";
 
 export function DefaultLayout() {
     return (
         <>
             <Header />
+            <main>
+                <Outlet />
+            </main>
             <Footer />
         </>
     )
