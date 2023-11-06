b7af2cd6fabf077a5373fa024338d9f30169d914
Pedro Henrique <phcosta069@gmail.com>
2023-11-06 20:54:56 -0300

    feat: SearchBar component include

--- src/pages/Home/components/SearchBar.tsx
+++ src/pages/Home/components/SearchBar.tsx
@@ -1,4 +1,23 @@
-export function SearchBar() {
+import { fetchProductsApi } from "../../../services/fetchProductsApi"
+import { SetStateAction, FormEvent, useState, useEffect } from "react"
+
+interface IPropsAtributtes {
+    setProductsData: React.Dispatch<SetStateAction<never[]>>
+}
+
+export function SearchBar({ setProductsData }: IPropsAtributtes) {
+    const [originalProductsData, setOriginalProductData] = useState([])
+
+    async function getProducts() {
+        const productsData = await fetchProductsApi()
+
+        setOriginalProductData(productsData.products)
+    }
+
+    useEffect(() => {
+        getProducts()
+    }, [])
+
     return (
         <>
             <input type="text" placeholder="What Are You Looking For?" />
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,7 +19,7 @@ export function Home() {
 
     return (
         <section>
-            <SearchBar />
+            <SearchBar setProductsData={setProductsData} />
             <Products productsData={productsData} />
         </section>
     )
