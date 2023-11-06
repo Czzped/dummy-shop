86a10736d0d2e18bf7318b20f3d1b41b128756c5
Pedro Henrique <phcosta069@gmail.com>
2023-11-06 17:11:31 -0300

    feat: Home SearchBar component include

---
+++ src/pages/Home/components/SearchBar.tsx
@@ -0,0 +1,8 @@
+export function SearchBar() {
+    return (
+        <>
+            <input type="text" placeholder="What Are You Looking For?" />
+            <br /><br />
+        </>
+    )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,10 @@
 import { Products } from "./components/Products";
+import { SearchBar } from "./components/SearchBar";
 
 export function Home() {
     return (
         <section>
+            <SearchBar />
             <Products />
         </section>
     )
