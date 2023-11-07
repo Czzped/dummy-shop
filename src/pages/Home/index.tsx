a147a154d319225a2305a51ac2e5c1e163b3ac7b
Pedro Henrique <phcosta069@gmail.com>
2023-11-06 21:59:21 -0300

    refactor: code in no use removed

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,7 @@
 import { Products } from "./components/Products";
 import { SearchBar } from "./components/SearchBar";
 
-import { fetchProductsApi } from "../../services/fetchProductsApi";
-import { useEffect, useState } from "react"
-
 export function Home() {
-    const [productsData, setProductsData] = useState([])
-
-    async function getProducts() {
-        const productsData = await fetchProductsApi()
-
-        setProductsData(productsData.products)
-    }
-
-    useEffect(() => {
-        getProducts()
-    }, [])
-
     return (
         <section>
             <SearchBar />
