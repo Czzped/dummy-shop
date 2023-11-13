ba27bcad87fb350bb840e58bf0ec7a13855e61d5
Pedro Henrique <phcosta069@gmail.com>
2023-11-13 16:52:36 -0300

    Home Dropdown component include

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,13 @@
+export function Dropdown() {
+    return (
+        <>
+            <select name="categories">
+                <option value="volvo">All</option>
+                <option value="saab">electronics</option>
+                <option value="fiat">jewelery</option>
+                <option value="audi">men's clothing</option>
+                <option value="audi">women's clothing</option>
+            </select >
+        </>
+    )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,14 @@
 import { Products } from "./components/Products";
 import { SearchBar } from "./components/SearchBar";
+import { Dropdown } from "../../components/DropDown";
 
 export function Home() {
     return (
         <section>
-            <SearchBar />
+            <div>
+                <SearchBar />
+                < Dropdown />
+            </div>
             <Products />
         </section>
     )
