a56a0a3e377319a7111f88593dab5b4a81e27b25
Pedro Henrique <phcosta069@gmail.com>
2023-11-13 17:24:48 -0300

    feat: Dropdown handleSelectCategoryChange function include

--- src/pages/Home/components/Dropdown.tsx
+++ src/pages/Home/components/Dropdown.tsx
@@ -1,12 +1,24 @@
+import { useProductsContext } from "../../../context/ProductsContext"
+
 export function Dropdown() {
+    const { resetProductsData, filterCategory } = useProductsContext()
+
+    function handleSelectCategoryChange(category: string) {
+        if (category === 'All') {
+            return resetProductsData()
+        }
+
+        filterCategory(category)
+    }
+
     return (
         <>
-            <select name="categories">
-                <option value="volvo">All</option>
-                <option value="saab">electronics</option>
-                <option value="fiat">jewelery</option>
-                <option value="audi">men's clothing</option>
-                <option value="audi">women's clothing</option>
+            <select onChange={(ev) => handleSelectCategoryChange(ev.currentTarget.value)} name="categories">
+                <option value="All">All</option>
+                <option value="electronics">electronics</option>
+                <option value="jewelery">jewelery</option>
+                <option value="men's clothing">men's clothing</option>
+                <option value="women's clothing">women's clothing</option>
             </select >
         </>
     )
