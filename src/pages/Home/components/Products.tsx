444fcad477ebf319475dcc6fcfbbb8a1576dae37
Pedro Henrique <phcosta069@gmail.com>
2023-11-29 14:51:28 -0300

    fix: Products component products data fixed

--- src/pages/Home/components/Products.tsx
+++ src/pages/Home/components/Products.tsx
@@ -1,27 +1,30 @@
 import { Link } from "react-router-dom"
-import { IProduct } from "../../../types/IProduct"
+import Stripe from "stripe";
 import { useProductsContext } from "../../../context/productsContext"
 import { Star, CurrencyDollar } from "phosphor-react";
 import { useProductsCartContext } from "../../../context/productsCartContext";
 
 
 export function Products() {
-    const { productsData } = useProductsContext()
+    const { products } = useProductsContext()
     const { addProductOnCart } = useProductsCartContext()
 
     return (
         <>
             {
-                productsData.length > 0 ?
-                    productsData.map((product: IProduct) => {
-                        const { id, title, image, price, rating } = product
+                products.length > 0 ?
+                    products.map((product: Stripe.Product) => {
+                        const { id, name, images, default_price, metadata } = product
+                        //@ts-ignore
+                        const price = (default_price.unit_amount) / 100
+
                         return (
                             <div key={id}>
-                                <img src={image} alt={`${title}-img`} />
-                                <h2>{title}</h2>
+                                <img src={images[0]} alt={`${name}-img`} />
+                                <h2>{name}</h2>
                                 <div>
                                     <h3><CurrencyDollar color="#108810" size={30} />{price}</h3>
-                                    <h3>{rating.rate}<Star size={30} color="#ffce00" weight="fill" /></h3>
+                                    <h3>{metadata.rating}<Star size={30} color="#ffce00" weight="fill" /></h3>
                                 </div>
                                 <div>
                                     <Link to={"/products/" + id}>
