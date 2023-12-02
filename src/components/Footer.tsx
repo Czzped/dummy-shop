df8c3f4525cd68a76d289fdb2f6f5c8c7f011d13
Pedro Henrique <phcosta069@gmail.com>
2023-12-01 23:12:22 -0300

    style: Footer new prop style include

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
     return (
-        <footer className="text-[1.2rem]">
+        <footer className="text-[1.1rem] text-center">
             <span>
                 Made With 💜 By <a href="https://github.com/Czzped" target="_blank">Pedro Henrique</a>
             </span>
