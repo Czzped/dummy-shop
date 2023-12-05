653826b57cd5ba7f3c2a3da6ad98f0b6439f92c4
Pedro Henrique <phcosta069@gmail.com>
2023-12-04 22:16:33 -0300

    style: ProductInformation style include

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ export function Button(props: { eventOnClick: () => void, buttonText: string, ad
     return (
         <button
             onClick={props.eventOnClick}
-            className={`flex justify-center font-semibold rounded bg-primaryColor ${props.additionalStyle} hover:opacity-80`}
+            className={`flex justify-center text-white font-semibold rounded bg-primaryColor ${props.additionalStyle} hover:opacity-80`}
         >
             {props.buttonText}
         </button>
--- src/pages/Product/components/ProductInformation.tsx
+++ src/pages/Product/components/ProductInformation.tsx
@@ -1,9 +1,13 @@
 import { useEffect } from "react"
-import { Link } from "react-router-dom";
-import { Star, CurrencyDollar } from "phosphor-react";
 import { useProductsContext } from "../../../context/productsContext";
 import { useProductsCartContext } from "../../../context/productsCartContext";
+
+import { Button } from "../../../components/Button";
+
+import { Star, CurrencyDollar } from "phosphor-react";
+
 import "react-toastify/dist/ReactToastify.css";
+
 import Stripe from "stripe";
 
 interface IProductInformationProps {
@@ -24,30 +28,35 @@ export function ProductInformation({ product }: IProductInformationProps) {
     }, [])
 
     return (
-        <section>
+        <section className="flex flex-col gap-4 p-4 lg:flex-row">
             <img src={images[0]} alt="product-thumbnail" />
-            <h1>
-                {name}
-            </h1>
-            <h3>
-                {metadata.rating} <Star size={30} color="#ffce00" weight="fill" />
-            </h3>
-            <b>{metadata.avaliationsAmount} avaliations</b>
-            <p>
-                {description}
-            </p>
-            <h1>
-                <CurrencyDollar color="#108810" size={30} /> {price}
-            </h1>
-            <div>
-                <Link to={"/"}>
-                    <button>
-                        back
-                    </button>
-                </Link>
-                <button onClick={() => addProductOnCart(product)}>
-                    add to the cart
-                </button>
+
+            <div className="flex flex-col gap-20 p-8">
+                <div className="flex flex-col gap-4">
+                    <div className="flex flex-col gap-4">
+                        <h1 className="text-4xl font-semibold">
+                            {name}
+                        </h1>
+
+                        <span className="text-moneyColor text-4xl">
+                            ${price}
+                        </span>
+                    </div>
+
+                    <div>
+                        <div className="flex gap-1 items-center">
+                            <Star color="#572C57" weight="fill" size={22} />
+                            <h2 className="text-[1.25rem] text-primaryColor">{metadata.rating}</h2>
+                        </div>
+                        <b>{metadata.avaliationsAmount} avaliations</b>
+                    </div>
+
+                    <p className="text-xl font-medium">
+                        {description}
+                    </p>
+                </div>
+
+                <Button eventOnClick={() => addProductOnCart(product)} buttonText="add to the cart" additionalStyle="p-4" />
             </div>
         </section >
     )
