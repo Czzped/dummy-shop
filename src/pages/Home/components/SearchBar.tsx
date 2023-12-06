a5fb4a1afbb8232545f96a38f1127300610f9d99
Pedro Henrique <phcosta069@gmail.com>
2023-12-06 12:31:18 -0300

    feat: SearchBar debounce function include

--- src/pages/Home/components/SearchBar.tsx
+++ src/pages/Home/components/SearchBar.tsx
@@ -1,19 +1,22 @@
-import { ChangeEvent } from "react"
 import { useProductsContext } from "../../../context/productsContext";
+
+import { debounce } from "lodash";
 
 export function SearchBar() {
     const { filterProducts } = useProductsContext()
 
-    function handleSearch(ev: ChangeEvent<HTMLInputElement>) {
-        filterProducts(ev.currentTarget.value.toLowerCase())
-    }
+    const handleDebounceSearch = (debounce((query: string) => {
+        filterProducts(query)
+    }, 500))
 
     return (
         <>
             <input
                 type="text"
                 placeholder="What Are You Looking For?"
-                onChange={(ev) => handleSearch(ev)}
+                onChange={(ev) => {
+                    handleDebounceSearch(ev.currentTarget.value)
+                }}
                 className="w-[75vw] p-2 outline-none text-xl border-b-2 duration-150 border-linesColor focus:border-primaryColor md:w-[40rem]"
             />
         </>
