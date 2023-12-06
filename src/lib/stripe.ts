b5f224a0a61b9b85a5c15cf28d5f971752e7e25e
Pedro Henrique <phcosta069@gmail.com>
2023-12-06 11:37:59 -0300

    refactor: code refactored pt3

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,29 +2,32 @@ import { loadStripe } from "@stripe/stripe-js";
 import Stripe from "stripe";
 
 let stripePromise: any
-
-if (!stripePromise) {
-    stripePromise = loadStripe(String(import.meta.env.VITE_STRIPE_PUBLIC_KEY))
-}
-
-const stripe = new Stripe(
-    import.meta.env.VITE_STRIPE_SECRET_KEY,
-    {
-        apiVersion: "2023-10-16",
-        appInfo: {
-            name: "Ignite Shop",
-        },
-    }
-);
-
-const { data } = await stripe.products.list({
-    expand: ["data.default_price"],
-    limit: 1000
-});
-
-const productsData = data.filter(product => product.metadata.available !== 'false')
+let productsData: any
 
 export async function getStripeData() {
+    if (!stripePromise) {
+        stripePromise = loadStripe(String(import.meta.env.VITE_STRIPE_PUBLIC_KEY))
+    }
+
+    if (!productsData) {
+        const stripe = new Stripe(
+            import.meta.env.VITE_STRIPE_SECRET_KEY,
+            {
+                apiVersion: "2023-10-16",
+                appInfo: {
+                    name: "Ignite Shop",
+                },
+            }
+        );
+
+        const { data } = await stripe.products.list({
+            expand: ["data.default_price"],
+            limit: 1000
+        });
+
+        productsData = data.filter(product => product.metadata.available !== 'false')
+    }
+
     return {
         stripePromise,
         productsData
