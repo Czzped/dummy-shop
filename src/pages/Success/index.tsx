d8feca4f4bc914e7ebc12458fdaa7fd90fe06f1d
Pedro Henrique <phcosta069@gmail.com>
2023-12-12 14:29:22 -0300

    style: Success component new styles include

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -2,13 +2,17 @@ import { Link } from "react-router-dom"
 
 import { Button } from "../../components/Button"
 
+import image from "../../assets/image.png"
+
 export function Success() {
     return (
         <section
-            className="flex flex-col items-center justify-center gap-16 h-full">
+            className="flex flex-col flex-1 items-center  justify-center text-center gap-8 h-full mb-28 p-4"
+        >
+            <img src={image} alt="completed-purchase-img" className="w-[30rem]" />
             <h1 className="text-3xl font-semibold">Congratulations, your purchase are completed!!</h1>
             <Link to={"/"}>
-                <Button buttonText="go back" additionalStyle="p-2" />
+                <Button buttonText="go back" additionalStyle="p-2 bg-green-800" />
             </Link>
         </section>
     )
