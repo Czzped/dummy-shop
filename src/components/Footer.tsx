4097bc778460a71e482b4d824aa6b8f5795fe04a
Pedro Henrique <phcosta069@gmail.com>
2024-01-12 15:57:47 -0300

    style: Footer component new style added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
     return (
         <footer className="text-[1.1rem] p-4 text-center">
             <span>
-                Made With 💜 By <a href="https://github.com/Czzped" target="_blank">Pedro Henrique</a>
+                Made With 💜 By <a href="https://github.com/Czzped" className="text-primaryColor font-semibold" target="_blank"> Pedro Henrique</a>
             </span>
         </footer >
     )
